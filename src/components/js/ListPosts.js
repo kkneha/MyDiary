5bd40bdf4358c460238b58d477632c03d1fd3bfc
Neha Kulkarni <nehawcbloom04@gmail.com>
2021-09-07 19:06:06 +0530

    DB connected

--- src/components/js/ListPosts.js
+++ src/components/js/ListPosts.js
@@ -1,38 +1,31 @@
-import React, {useEffect, useState} from 'react'
-import SinglePost from './SinglePost'
+import React, { useEffect, useState } from "react";
+import SinglePost from "./SinglePost";
 
 export default function ListPosts() {
-    const [postList,setPostList] = useState([])
-    // const baseURL = 'http://127.0.0.1:8000/post/'
-    const baseURL = 'https://p-diary.herokuapp.com/post/'
-    
-    useEffect(() => {                   // useEffect -> loading the data once per refresh
-        fetch(baseURL + 'get-posts/')   // fecth data from server at the given endpoint
-        .then(response => {
-            if(response.ok) {
-                return response.json()  // code 200 then convert to json format
-            }
-            throw response              // error code 404 or 500 throw error
-        })
-        .then(data => {
-            setPostList(data)           // stores the data recieved from server to list
-        })
-    },[])
+  const [postList, setPostList] = useState([]);
+  // const baseURL = 'http://127.0.0.1:8000/post/'
+  const baseURL = "https://p-diary.herokuapp.com/post/";
 
-/*     return (
-        <div>
-            {postList && (postList.map((data,index) => {
-                console.log(postList)
-                return <h1 key={index}>{data['fields']['title']}</h1>
-            }))}
-        </div>
-    ) */
+  useEffect(() => {
+    // useEffect -> loading the data once per refresh
+    fetch(baseURL + "get-posts/") // fecth data from server at the given endpoint
+      .then((response) => {
+        if (response.ok) {
+          return response.json(); // code 200 then convert to json format
+        }
+        throw response; // error code 404 or 500 throw error
+      })
+      .then((data) => {
+        setPostList(data); // stores the data recieved from server to list
+      });
+  }, []);
 
-    return (
-        <div>
-           <SinglePost/>
-           <SinglePost/>
-           <SinglePost/>
-        </div>
-    )
+  return (
+    <div>
+      {postList &&
+        postList.map((data, index) => {
+          return <SinglePost key={index} data={data["fields"]} />;
+        })}
+    </div>
+  );
 }
--- src/components/js/SinglePost.js
+++ src/components/js/SinglePost.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import "../css/SinglePost.css"
 
-export default function SinglePost() {
+export default function SinglePost({data}) {
+    
+    const months = ["January", "February", "March", "April", "May", "June", "July", 
+                    "August", "September", "October", "November", "December"]; 
+
+    var currentMonth = months[parseInt(data['createdOn'].toString().slice(5, 7))-1]
+    var currentDate = data['createdOn'].toString().slice(8,10)
+    var currentYear = data['createdOn'].toString().slice(0,4)
+    
     return (
         <div className="post">
-            <h4 className="date">23rd August 2021</h4>
-            <h3 className="title">My First Day at college</h3>
+            <h4 className="date">{currentDate} {currentMonth} {currentYear}</h4>
+            <h3 className="title">{data['title']}</h3>
             <p className="body">
-                Whose woods these are I think I know.   
-                His house is in the village though ....
+                {data['body'].slice(0, 10)}....
             </p>
         </div>
     )

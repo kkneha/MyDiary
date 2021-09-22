a1ef2018c2d51a13e21c3e012445af14f792c390
Rahul Barman <rahulbarman1215@gmail.com>
2021-09-22 16:16:25 +0530

    added link intead of reload and darken class

--- src/components/js/Popup.js
+++ src/components/js/Popup.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import "../css/Popup.css"
-export default function Popup(typeFor) {
-    function closePopup(){
-        window.location.assign("/")
-    }
+import {Link} from 'react-router-dom'
+
+export default function Popup() {
     return (
         <div className="popup-wrapper">
         <div className="popup-box">
             <div className="box">
                 <h5 className="popup-text">Done!</h5>
-                <button className="popup-btn" onClick={closePopup}>Okay</button>
+                <Link to="/">
+                    <button className="popup-btn">Okay</button>
+                </Link>
             </div>
         </div>
         </div>
